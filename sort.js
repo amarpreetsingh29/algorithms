208e2b0a1ea741f90fcfb5b3c5bf4d7ae6c8c261
gs-samarpreet <samarpreet@gainsight.com>
2017-03-27 08:12:11 +0530

    Implement Sorting

---
+++ sort.js
@@ -0,0 +1,16 @@
+/**
+ * Created by samarpreet on 27/03/17.
+ */
+
+import smallest from './smallest';
+
+function tSort(input,result) {
+    var result = result || [];
+    for(let i=0; i<input.length; i++){
+       let elm = smallest(input,result);
+        input.splice(input.indexOf(elm),1);
+        result.push(elm);
+        tSort(input,result);
+    }
+    return result;
+}
