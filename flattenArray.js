a0ceb66eff5d17765804718ae2c54dd0875a007c
gs-samarpreet <samarpreet@gainsight.com>
2017-09-18 19:03:22 +0530

    using instance of

--- flattenArray.js
+++ flattenArray.js
@@ -2,20 +2,18 @@
  * Created by samarpreet on 26/03/17.
  */
 
-/**
- *
- *
- * @param input
- */
 
-export default function flattenArray(input,result){
+function flattenArray(input, result) {
     var result = result || [];
-    for(let i =0; i < input.length;i++){
-        if(typeof  input[i] !== "object"){
+    for (let i = 0; i < input.length; i++) {
+        if (input[i] instanceof Array) {
+            flattenArray(input[i], result);
+        } else {
             result.push(input[i]);
-        }else {
-            flattenArray(input[i],result);
         }
     }
     return result;
 }
+
+var t = flattenArray([1,2,[3,4,[5,6]]]);
+console.log(t);
